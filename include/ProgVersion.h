d98852030e885bc39aaac2c93f8d56381cc25b12
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2026-07-12 13:33:46 +0200

    Version 2.3.0.1

--- include/ProgVersion.h
+++ include/ProgVersion.h
@@ -2,16 +2,16 @@
 // (c) LSC Labs
 // This file will be build by the runtime, so do not change here.
 // If you want to change the major/minor/patch version nummer, use the file data/prog_ver.json.
-// Use the npm incr* scripts to update this file.
-// The build nummer will be incremented by platform.io -> prebuild steps (release mode)
+// The build nummer will be incremented in this file by platform.io -> prebuild steps
 
 #define PROG_VERSION_NAME  "OnAir Light"
-#define PROG_AUTHOR        LSC Labs - P.Liebl
-#define PROG_HOMEPAGE      https://github.com/lsc-labs/OnAir-Light#readme
+#define PROG_AUTHOR        "LSC Labs - P.Liebl"
+#define PROG_HOMEPAGE      "https://github.com/lsc-labs/OnAir-Light#readme"
+
 #define PROG_VERSION_MAJOR 2
 #define PROG_VERSION_MINOR 3
 #define PROG_VERSION_PATCH 0
-#define PROG_VERSION_BUILD 0
+#define PROG_VERSION_BUILD 1
 #define PROG_VERSION       "2.3.0"
 #define PROG_VERSION_SHORT "2.3"
-#define PROG_VERSION_LONG  "2.3.0.0"
+#define PROG_VERSION_LONG  "2.3.0.1"
